798a7750b51a7b5ce45b2e2558339b8165201e28
salling008 <frederiksalling8@hotmail.com>
2019-11-21 20:40:04 +0100

    IT07 Done

--- Microwave.Test.Integration/IT07_UserInterface_Display.cs
+++ Microwave.Test.Integration/IT07_UserInterface_Display.cs
@@ -1,12 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
 
 namespace Microwave.Test.Integration
 {
     class IT07_UserInterface_Display
     {
+        public IOutput _output;
+        public IPowerTube _powerTube;
+        public ICookController _cookController;
+        public IButton _buttonPower;
+        public IButton _buttonTime;
+        public IButton _buttonStartCancle;
+        public IDoor _door;
+        public ILight _light;
+
+
+        public Display _display;
+        public Timer _timer;
+        public UserInterface _sut;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cookController = Substitute.For<ICookController>();
+            _powerTube = Substitute.For<IPowerTube>();
+            _output = Substitute.For<IOutput>();
+            _buttonPower = Substitute.For<IButton>();
+            _buttonTime = Substitute.For<IButton>();
+            _buttonStartCancle = Substitute.For<IButton>();
+            _door = Substitute.For<IDoor>();
+            _light = Substitute.For<ILight>();
+
+            _timer = new Timer();
+            _display = new Display(_output);
+            _sut = new UserInterface(_buttonPower, _buttonTime, _buttonStartCancle, _door, _display, _light, _cookController);
+        }
+
+        [Test]
+        public void PowerButton_Pressed_ShowPower_Called()
+        {
+            _buttonPower.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            _output.Received(1).OutputLine("Display shows: 50 W");
+        }
+
+        [Test]
+        public void TimeButton_Pressed_ShowTime_Called()
+        {
+            _buttonPower.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            _buttonTime.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            _output.Received(1).OutputLine("Display shows: 01:00");
+        }
+
+        [Test]
+        public void CookingIsDone_Clear_Called()
+        {
+            _buttonPower.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            _buttonTime.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            _buttonStartCancle.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            _sut.CookingIsDone();
+
+            // Now in SetTime
+            _output.Received(1).OutputLine("Display cleared");
+        }
+
+        [Test]
+        public void Door_Open_When_Cooking_Clear_Called()
+        {
+            _buttonPower.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            _buttonTime.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            _buttonStartCancle.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            
+            _door.Opened += Raise.EventWith(this, EventArgs.Empty);
+
+            // Now in SetTime
+            _output.Received(1).OutputLine("Display cleared");
+        }
+
+        [Test]
+        public void ButtonStartCancle_Pressed_When_Cooking_Clear_Called()
+        {
+            _buttonPower.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            _buttonTime.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            _buttonStartCancle.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            _buttonStartCancle.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            // Now in SetTime
+            _output.Received(1).OutputLine("Display cleared");
+        }
+
     }
 }
